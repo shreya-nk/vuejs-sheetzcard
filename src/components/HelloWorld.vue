16ef76b02a0cf1f5109d45b03bf777b6f28dc1a5
shreya-nk <shreya.kakumani@gmail.com>
2023-12-13 19:22:15 -0500

    change files

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,143 +1,41 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+<div class="card">
+  <div class="logo">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/0/02/Sheetz_logo.svg" alt="logo" class="logo"/>
   </div>
-  <div class="card">
-    <img alt="real estate logo" src="./housepic.jpg">
-    <div class="card-content"> 
-      <h2>Two Pod Realty</h2> 
-      <p id="description">Real Estate Company Focused on delivering the best properties in the State College Area</p> 
-      <button class="details-button" id="detailsBtn">Details</button>
-    </div> 
-  </div> 
-  
-  <button id="btn">Duplicator</button>
-  <button id="colorBtn">BGColorChanger</button>
-  <button id="Headingbtn">HeadingChanger</button>
-  <button id="dltbtn">DeleteCard</button>
+  <details>
+<summary> Detail </summary>
+<p>
+  Sheetz is a chain of convenience stores and gas stations. Its services include convenience store items, gasoline and fuel services, and food.
+</p>
+</details>
+<div class='directions'>
+<a href="https://www.google.com/maps/place/Sheetz/@40.7844937,-77.8317491,15z/data=!4m6!3m5!1s0x89ceaf223c5770c5:0x7c69de1401daf29f!8m2!3d40.7844937!4d-77.8317491!16s%2Fg%2F11gzn1143_?entry=ttu">Directions</a>
+</div>
+
+<h1> Most Popular Item: </h1>
+<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Hot_dog_with_mustard.png/440px-Hot_dog_with_mustard.png" alt="hotdog"/>
+
+
+</div>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  },
-  mounted() {
-    document.querySelector('#btn').addEventListener('click', () => {
-      const clone = document.querySelector('.card').cloneNode(true); // Clone the whole card
-      document.body.appendChild(clone);
-    });
-    
-    var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-    var letters = ["A", "B", "C", "D", "E", "F"];
-    
-    function randomColor() {
-      var color = "";
-      for (var i = 0; i < 3; i++) {
-        var index = Math.floor(Math.random() * (numbers.length + letters.length));
-        color += (index < numbers.length) ? numbers[index] : letters[index - numbers.length];
-      }
-      return color;
-    }
-    
-    document.querySelector('#colorBtn').addEventListener('click', () => {
-      const card = document.querySelector('.card');
-      // Change background color
-      card.style.backgroundColor = '#' + randomColor();
-    });
-    
-    // HEADING CHANGER
-    document.querySelector('#Headingbtn').addEventListener('click', () => {
-      const title = document.querySelector('h2');
-      title.innerText = "Super Pod 2 Realty";
-    });
-    
-    // Card delete
-    document.querySelector('#dltbtn').addEventListener('click', () => {
-      const cards = document.querySelectorAll('.card');
-      // Check if there are cards to delete
-      if (cards.length > 1) {
-        const lastCard = cards[cards.length - 1];
-        lastCard.parentNode.removeChild(lastCard);
-      }
-    });
-    
-    var original = document.querySelector('#description').innerText;
-    
-    document.querySelector('#detailsBtn').addEventListener('click', () => {
-      const description = document.querySelector('#description');
-      // Toggle the visibility of the description
-      if (description.innerText === 'none' || description.innerText === '') {
-        description.innerText = original; // Show the description
-      } else {
-        description.innerText = ''; // Hide the description
-      }
-    });
-  }
-}
-</script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 .card {
-  max-width: 400px;
+  width: 420px;
   margin: 0 auto;
-  border: 1px solid #0C0B0B;
-  background-color: #0C0B0B;
-  padding: 16px;
-  display: flex;
-  flex-direction: column;
+  border: 1px solid;
+  padding: 10px;
+  text-align: center;
+  background-color: rgb(232, 123, 123);
+}
+
+.h1 {
   text-align: center;
 }
 
-.card img {
-  max-width: 100%;
-  height: auto;
-}
 
-.card-content {
-  flex: 1;
-}
 
-/* Style for the Title label */
-h2 {
-  font-size: 24px;
-  margin: -10px 0;
-  /* Background color and border */
-  background-color: #F8471B;
-  /* Background color */
-  color: #fff;
-  /* Text color on the background */
-  padding: 8px 16px;
-  border-radius: 4px;
-}
-
-p {
-  font-size: 16px;
-  margin: 8px 0;
-  color: #fff;
-}
-
-a.details-button {
-  background-color: #EA5F0A;
-  color: #fff;
-  padding: 8px 16px;
-  text-decoration: none;
-  border-radius: 4px;
-}
 </style>
